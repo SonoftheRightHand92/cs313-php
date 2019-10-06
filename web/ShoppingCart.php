7eb92e27ac48bee9f04b569c263f86ba34a321d8
Benjamin Thaut <dasaxmansbrother@hotmail.com>
2019-10-05 22:48:38 -0600

    stuff

--- web/ShoppingCart.php
+++ web/ShoppingCart.php
@@ -28,7 +28,7 @@
         for ($i = 0; $i < sizeof($meat); $i++) {
             if (!is_null($meat[$i])) {
                 $meatID = "A0" . $i;
-                $text = $text . "<input type='text' id='" . $meatID . "' value='". $meat[$i] ."' readonly>" . $meatID . " <button type='button' onclick='myFunction($meatID)'>Remove</button><br><br>";
+                $text = $text . "<input type='text' id='" . (string)$meatID . "' value='". $meat[$i] ."' readonly>" . $meatID . " <button type='button' onclick='myFunction(A0" . $i . ")'>Remove</button><br><br>";
             }
         }
 
