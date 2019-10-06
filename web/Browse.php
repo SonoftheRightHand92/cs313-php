8972516f2b05214aace9e2cfb9326880f03c0ac3
Benjamin Thaut <dasaxmansbrother@hotmail.com>
2019-10-05 20:55:26 -0600

    stuff

--- web/Browse.php
+++ web/Browse.php
@@ -89,10 +89,10 @@
         }
 
         if (is_null($var) || is_null($var2) || is_null($var3)) {
-            echo "<br>Items in Cart:<br> 
-            <textarea id='area' rows='14' cols='50' readonly>" .
+            // echo "<br>Items in Cart:<br> 
+            // <textarea id='area' rows='14' cols='50' readonly>" .
             
-            . "</textarea><br>"
+            // . "</textarea><br>"
         }
 
         $_SESSION["m"] = $_POST['meat'];
