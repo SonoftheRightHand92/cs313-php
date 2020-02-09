1d8695f3e8916f48e88c2290c4766c5958cbdd89
Benjamin Thaut <dasaxmansbrother@hotmail.com>
2020-02-08 17:07:32 -0700

    asdf

--- web/sql/getTable.php
+++ web/sql/getTable.php
@@ -6,11 +6,11 @@
    while ($cRow = $comments->fetch(PDO::FETCH_ASSOC))
    {
       $custom_name = $fRow["custom_name"];
-      echo "Custom Name: $custom_name<br>"
+      echo "Custom Name: $custom_name<br>";
       $user_comment = $fRow["user_comment"];
-      echo "User comment: $user_comment<br>"
+      echo "User comment: $user_comment<br>";
       $user_description = $fRow["user_description"];
-      echo "User description: $user_description<br>"
+      echo "User description: $user_description<br>";
       echo "<p>$custom_name: $user_comment<br>About $custom_name: $user_description</p>";
    }
 ?>
