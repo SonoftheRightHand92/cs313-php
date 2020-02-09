a9bf206763041dfa6858671ab2ad2aec732380bd
Benjamin Thaut <dasaxmansbrother@hotmail.com>
2020-02-08 17:40:42 -0700

    asdf

--- web/sql/getTable.php
+++ web/sql/getTable.php
@@ -8,8 +8,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Comments</title>
+
+    <style>
+        h1 {
+            text-align: center;
+        }
+    </style>
 </head>
 <body>
+    <h1>Comments</h1>
 <?php
    require "dbConnect.php";
    $db = get_db();
