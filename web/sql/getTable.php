badaf260df1c5515998534df5bd6f6b72ad2a380
Benjamin Thaut <dasaxmansbrother@hotmail.com>
2020-02-08 17:26:07 -0700

    asdf

--- web/sql/getTable.php
+++ web/sql/getTable.php
@@ -18,30 +18,11 @@
       $custom_name = $cRow["custom_name"];
       $user_comment = $cRow["user_comment"];
       $user_description = $cRow["user_description"];
-      echo "<p>$custom_name: $user_comment<br>About $custom_name: $user_description</p>";
+      echo "<p>$custom_name:<br><textarea rows='3' cols='80' readonly>$user_comment</textarea><br>About $custom_name: $user_description</p>";
    }
 ?>
 </body>
 </html>
-
-
-<!-- //
-//    require "dbConnect.php";
-//    $db = get_db();
-//    $comments = $db->prepare("SELECT * FROM comments");
-
-//    $comments->execute();
-//    while ($cRow = $comments->fetch(PDO::FETCH_ASSOC))
-//    {
-//       $custom_name = $cRow["custom_name"];
-//       echo "Custom Name: $custom_name<br>";
-//       $user_comment = $cRow["user_comment"];
-//       echo "User comment: $user_comment<br>";
-//       $user_description = $cRow["user_description"];
-//       echo "User description: $user_description<br>";
-//       echo "<p>$custom_name: $user_comment<br>About $custom_name: $user_description</p>";
-//    } -->
-
 
 
 <!-- <textarea rows='14' cols='50' readonly> -->
