e3fb53ac403357d597334407efe99f1bb80b17f6
Benjamin Thaut <dasaxmansbrother@hotmail.com>
2020-02-22 17:56:19 -0700

    stuff

--- web/project01/views/insert.php
+++ web/project01/views/insert.php
@@ -1,3 +1,6 @@
+<?php
+	session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -38,67 +41,50 @@ for ($i = 0; $i < sizeof($housing); $i++) {
 	}
 }
 
-if (isset($_SESSION["email"])) {
-	echo $email;
-}
-else {
-	echo "no email";
-}
-
-require("dbConnect.php");
-$db = get_db();
-
 try
 {
-	echo "<h1>HELLO $email</h1>";
-	if (isset($_SESSION["email"])) {
-		echo $email;
-	}
-	else {
-		echo "no email";
-	}
-	// $query = "INSERT INTO renters (first_name, last_name, email, street_address, city, state_name, zip, area_code_home, first_three_home, last_four_home, area_code_cell, first_three_cell, last_four_cell, b_month, b_day, b_year, housing_contract) VALUES (:firstName, :lastName, :email, :street, :city, :state, :zip, :areaCode, :next3, :last4, :cellAreaCode, :cellNext3, :cellLast4, :b_month, :b_day, :b_year, :string)";
-	// $statement = $db->prepare($query);
-	// $statement->bindValue(':firstName', $firstName);
-	// $statement->bindValue(':lastName', $lastName);
-	// $statement->bindValue(':email', $email);
-	// $statement->bindValue(':street', $street);
-	// $statement->bindValue(':city', $city);
-	// $statement->bindValue(':state', $state);
-	// $statement->bindValue(':zip', $zip);
-	// $statement->bindValue(':areaCode', $areaCode);
-	// $statement->bindValue(':next3', $next3);
-	// $statement->bindValue(':last4', $last4);
-	// $statement->bindValue(':cellAreaCode', $cellAreaCode);
-	// $statement->bindValue(':cellNext3', $cellNext3);
-	// $statement->bindValue(':cellLast4', $cellLast4);
-	// $statement->bindValue(':b_month', $b_month);
-	// $statement->bindValue(':b_day', $b_day);
-	// $statement->bindValue(':b_year', $b_year);
-	// $statement->bindValue(':string', $string);
-	// $statement->execute();
+	$query = "INSERT INTO renters (first_name, last_name, email, street_address, city, state_name, zip, area_code_home, first_three_home, last_four_home, area_code_cell, first_three_cell, last_four_cell, b_month, b_day, b_year, housing_contract) VALUES (:firstName, :lastName, :email, :street, :city, :state, :zip, :areaCode, :next3, :last4, :cellAreaCode, :cellNext3, :cellLast4, :b_month, :b_day, :b_year, :string)";
+	$statement = $db->prepare($query);
+	$statement->bindValue(':firstName', $firstName);
+	$statement->bindValue(':lastName', $lastName);
+	$statement->bindValue(':email', $email);
+	$statement->bindValue(':street', $street);
+	$statement->bindValue(':city', $city);
+	$statement->bindValue(':state', $state);
+	$statement->bindValue(':zip', $zip);
+	$statement->bindValue(':areaCode', $areaCode);
+	$statement->bindValue(':next3', $next3);
+	$statement->bindValue(':last4', $last4);
+	$statement->bindValue(':cellAreaCode', $cellAreaCode);
+	$statement->bindValue(':cellNext3', $cellNext3);
+	$statement->bindValue(':cellLast4', $cellLast4);
+	$statement->bindValue(':b_month', $b_month);
+	$statement->bindValue(':b_day', $b_day);
+	$statement->bindValue(':b_year', $b_year);
+	$statement->bindValue(':string', $string);
+	$statement->execute();
 	
-	// $db->lastInsertId("renters_id_seq");
+	$db->lastInsertId("renters_id_seq");
 
-	// $query2 = "INSERT INTO profiles (custom_name, code, email)
-	// VALUES (:userName, :password, :email)";
+	$query2 = "INSERT INTO profiles (custom_name, code, email)
+	VALUES (:userName, :password, :email)";
 
-	// $statement2 = $db->prepare($query2);
-	// $statement2->bindValue(':userName', $userName);
-	// $statement2->bindValue(':password', $password);
-	// $statement2->bindValue(':email', $email);
-	// $statement2->execute();
+	$statement2 = $db->prepare($query2);
+	$statement2->bindValue(':userName', $userName);
+	$statement2->bindValue(':password', $password);
+	$statement2->bindValue(':email', $email);
+	$statement2->execute();
 
-	// $db->lastInsertId("profiles_id_seq");
+	$db->lastInsertId("profiles_id_seq");
 }
 catch (Exception $ex)
 {
 	echo "Error with DB. Details: $ex";
 	die();
 }
-// header("Location: sign_in.php");
+header("Location: sign_in.php");
 
-// die(); 
+die(); 
 ?>
 
 </html>
