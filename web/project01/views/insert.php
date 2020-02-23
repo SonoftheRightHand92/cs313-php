6ed2aed72332ab12cffc50b7f16cd8db44c4427d
Benjamin Thaut <dasaxmansbrother@hotmail.com>
2020-02-22 18:03:15 -0700

    stuff

--- web/project01/views/insert.php
+++ web/project01/views/insert.php
@@ -31,39 +31,39 @@ for ($i = 0; $i < sizeof($housing); $i++) {
 
 try
 {
-	$query = "INSERT INTO renters (first_name, last_name, email, street_address, city, state_name, zip, area_code_home, first_three_home, last_four_home, area_code_cell, first_three_cell, last_four_cell, b_month, b_day, b_year, housing_contract) VALUES (:firstName, :lastName, :email, :street, :city, :state, :zip, :areaCode, :next3, :last4, :cellAreaCode, :cellNext3, :cellLast4, :b_month, :b_day, :b_year, :string)";
-	$statement = $db->prepare($query);
-	$statement->bindValue(':firstName', $firstName);
-	$statement->bindValue(':lastName', $lastName);
-	$statement->bindValue(':email', $email);
-	$statement->bindValue(':street', $street);
-	$statement->bindValue(':city', $city);
-	$statement->bindValue(':state', $state);
-	$statement->bindValue(':zip', $zip);
-	$statement->bindValue(':areaCode', $areaCode);
-	$statement->bindValue(':next3', $next3);
-	$statement->bindValue(':last4', $last4);
-	$statement->bindValue(':cellAreaCode', $cellAreaCode);
-	$statement->bindValue(':cellNext3', $cellNext3);
-	$statement->bindValue(':cellLast4', $cellLast4);
-	$statement->bindValue(':b_month', $b_month);
-	$statement->bindValue(':b_day', $b_day);
-	$statement->bindValue(':b_year', $b_year);
-	$statement->bindValue(':string', $string);
-	$statement->execute();
+	// $query = "INSERT INTO renters (first_name, last_name, email, street_address, city, state_name, zip, area_code_home, first_three_home, last_four_home, area_code_cell, first_three_cell, last_four_cell, b_month, b_day, b_year, housing_contract) VALUES (:firstName, :lastName, :email, :street, :city, :state, :zip, :areaCode, :next3, :last4, :cellAreaCode, :cellNext3, :cellLast4, :b_month, :b_day, :b_year, :string)";
+	// $statement = $db->prepare($query);
+	// $statement->bindValue(':firstName', $firstName);
+	// $statement->bindValue(':lastName', $lastName);
+	// $statement->bindValue(':email', $email);
+	// $statement->bindValue(':street', $street);
+	// $statement->bindValue(':city', $city);
+	// $statement->bindValue(':state', $state);
+	// $statement->bindValue(':zip', $zip);
+	// $statement->bindValue(':areaCode', $areaCode);
+	// $statement->bindValue(':next3', $next3);
+	// $statement->bindValue(':last4', $last4);
+	// $statement->bindValue(':cellAreaCode', $cellAreaCode);
+	// $statement->bindValue(':cellNext3', $cellNext3);
+	// $statement->bindValue(':cellLast4', $cellLast4);
+	// $statement->bindValue(':b_month', $b_month);
+	// $statement->bindValue(':b_day', $b_day);
+	// $statement->bindValue(':b_year', $b_year);
+	// $statement->bindValue(':string', $string);
+	// $statement->execute();
 	
-	$db->lastInsertId("renters_id_seq");
+	// $db->lastInsertId("renters_id_seq");
 
-	// $query2 = "INSERT INTO profiles (custom_name, code, email)
-	// VALUES (:userName, :password, :email)";
+	$query2 = "INSERT INTO profiles (custom_name, code, email)
+	VALUES (:userName, :password, :email)";
 
-	// $statement2 = $db->prepare($query2);
-	// $statement2->bindValue(':userName', $userName);
-	// $statement2->bindValue(':password', $password);
-	// $statement2->bindValue(':email', $email);
-	// $statement2->execute();
+	$statement2 = $db->prepare($query2);
+	$statement2->bindValue(':userName', $userName);
+	$statement2->bindValue(':password', $password);
+	$statement2->bindValue(':email', $email);
+	$statement2->execute();
 
-	// $db->lastInsertId("profiles_id_seq");
+	$db->lastInsertId("profiles_id_seq");
 }
 catch (Exception $ex)
 {
